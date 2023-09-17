bed7426dc74441d920a732556566fac2121404f8
mateuscardososs <100202320+mateuscardososs@users.noreply.github.com>
2023-09-17 22:17:39 +0000

    commit

--- Usuario.java
+++ Usuario.java
@@ -20,4 +20,73 @@ public class Usuario {
         this.decks = decks;
         this.pontos = pontos;
     }
+
+    public String getNick() {
+        return nick;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public int getIdade() {
+        return idade;
+    }
+    public String getSexo() {
+        return sexo;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public int getNivel() {
+        return nivel;
+    }
+    public String[] getDecks() {
+        return decks;
+    }
+    public double getPontos() {
+        return pontos;
+    }
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+    public void setDecks(String[] decks) {
+        this.decks = decks;
+    }
+    public void setPontos(double pontos) {
+        this.pontos = pontos;
+    }
+    public String toString() {
+        return "Usuario{" +
+                "nick='" + nick + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", senha='" + senha + '\'' +
+                ", idade=" + idade +
+                ", sexo='" + sexo + '\'' +
+                ", email='" + email + '\'' +
+                ", nivel=" + nivel +
+                ", decks=" + Arrays.toString(decks) +
+                ", pontos=" + pontos +
+                '}';
+    }
+
 }
