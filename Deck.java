173beb42e16695ce1eee2891a5195d64907c1174
gustavocarneiro453 <gustavocidc@gmail.com>
2023-09-18 02:55:25 +0000

    Atividade em andamento

--- Deck.java
+++ Deck.java
@@ -1,3 +1,11 @@
 public class Deck {
+    private String nome;
+    private String Qdcartas;
+    private boolean disponibilidade;
+
+    public Deck()
+    this.nome = nome;
     
+    this.disponibilidade = false;
+
 }
