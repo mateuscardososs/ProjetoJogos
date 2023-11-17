19a7684c430d3eb5acf5ac2a1b6a5337ef9cf6d8
Breno Oliveira <99503617+Brelin27@users.noreply.github.com>
2023-11-17 00:42:39 +0000

    teste

--- Loja.java
+++ Loja.java
@@ -1,50 +1,51 @@
 import java.util.Random;
 
-public class Loja {
+public class Loja extends Inventario {
     private String numeroCartao;
     private String codigoVerificador;
     private boolean promocao;
 
-    public Loja(String numeroCartao, String codigoVerificador, boolean promocao) {
+    public Loja(String numeroCartao, String codigoVerificador, boolean promocao, int cardcoins, List<Carta> cartas) {
+        super(cardcoins, cartas);
         this.numeroCartao = numeroCartao;
         this.codigoVerificador = codigoVerificador;
         this.promocao = promocao;
     }
 
-    public void compraBooster(Inventario inventario) {
+    public void compraBooster() {
         if (promocao) {
-            compraBoosterEspecial(inventario);
+            compraBoosterEspecial();
         } else {
-            compraBooster(inventario);
+            compraBooster();
         }
     }
 
-    private void compraBooster(Inventario inventario) {
+    private void compraBooster() {
         int custoBooster = 120;
 
-        if (inventario.getCardcoins() >= custoBooster) {
-            inventario.setCardcoins(inventario.getCardcoins() - custoBooster);
-            adicionarCartasAleatoriasAoInventario(inventario);
+        if (getCardcoins() >= custoBooster) {
+            setCardcoins(getCardcoins() - custoBooster);
+            adicionarCartasAleatoriasAoInventario();
         } else {
             System.out.println("Saldo insuficiente de cardcoins para o Booster.");
         }
     }
 
-    private void compraBoosterEspecial(Inventario inventario) {
+    private void compraBoosterEspecial() {
         int custoBoosterEspecial = 150;
 
-        if (inventario.getCardcoins() >= custoBoosterEspecial) {
-            inventario.setCardcoins(inventario.getCardcoins() - custoBoosterEspecial);
-            
+        if (getCardcoins() >= custoBoosterEspecial) {
+            setCardcoins(getCardcoins() - custoBoosterEspecial);
+
             Random random = new Random();
             int quantidadeCartasNoBooster = 12;
 
             for (int i = 0; i < quantidadeCartasNoBooster; i++) {
                 if (random.nextInt(100) < 1) {
                     Carta cartaUnica = gerarCartaAleatoria();
-                    inventario.adicionarCarta(cartaUnica);
+                    adicionarCarta(cartaUnica);
                 } else {
-                    adicionarCartasAleatoriasAoInventario(inventario);
+                    adicionarCartasAleatoriasAoInventario();
                 }
             }
         } else {
@@ -52,27 +53,26 @@ public class Loja {
         }
     }
 
-    private void adicionarCartasAleatoriasAoInventario(Inventario inventario) {
+    private void adicionarCartasAleatoriasAoInventario() {
         Random random = new Random();
         int quantidadeCartasNoBooster = 12;
 
         for (int i = 0; i < quantidadeCartasNoBooster; i++) {
-
             Carta cartaAleatoria = gerarCartaAleatoria();
 
-            if (inventario.contagemCarta(cartaAleatoria) < 3) {
-                inventario.adicionarCarta(cartaAleatoria);
+            if (contagemCarta(cartaAleatoria) < 3) {
+                adicionarCarta(cartaAleatoria);
             } else {
-                int valorCardcoins = 10; 
-                inventario.setCardcoins(inventario.getCardcoins() + valorCardcoins);
+                int valorCardcoins = 10;
+                setCardcoins(getCardcoins() + valorCardcoins);
             }
         }
     }
 
     private Carta gerarCartaAleatoria() {
-        String[] nomesCartas = {"Carta1", "Carta2", "Carta3", "Carta4", "Carta5"};/
+        String[] nomesCartas = {"Carta1", "Carta2", "Carta3", "Carta4", "Carta5"};
         Random random = new Random();
         String nomeCartaAleatoria = nomesCartas[random.nextInt(nomesCartas.length)];
         return new Carta(nomeCartaAleatoria, nomeCartaAleatoria, nomeCartaAleatoria, nomeCartaAleatoria, 0, 0, 0, nomeCartaAleatoria, 0);
     }
-    }
+}
