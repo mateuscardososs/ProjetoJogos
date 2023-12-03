a48e41326f7d6fd0a548411293c8ca967f2759a4
Breno Oliveira <99503617+Brelin27@users.noreply.github.com>
2023-12-03 02:05:11 +0000

    Comentarios

--- CartaUnique.java
+++ CartaUnique.java
@@ -2,11 +2,12 @@ import java.util.Random;
 
 public class CartaUnique extends Carta {
 
+        // Construtor
     public CartaUnique(String nome, String imagem, String tipo, String raridade, int ataque, int defesa, int custo, String habilidade, int quantidade) {
         super(nome, imagem, tipo, raridade, ataque + 1, defesa + 1, custo, habilidade, quantidade);
         adicionarHabilidadeExtra();
     }
-
+      // Adiciona uma habilidade extra de forma aleatória
     private void adicionarHabilidadeExtra() {
         
         String[] habilidadesExtras = {"VELOCIDADE,GELO,SUPERPULO,MAGIA"};
@@ -15,10 +16,10 @@ public class CartaUnique extends Carta {
         String habilidadeExtra = habilidadesExtras[indiceHabilidadeExtra];
         setHabilidade(getHabilidade() + ", " + habilidadeExtra);
     }
-
+     // Define as habilidades da carta
     private void setHabilidade(String string) {
     }
-
+    // Obtém as habilidades da carta
     private String getHabilidade() {
         return null;
     }
