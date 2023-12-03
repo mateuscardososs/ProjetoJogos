930cf13f0f539bbf76234b0aca6a081715a51171
Breno Oliveira <99503617+Brelin27@users.noreply.github.com>
2023-12-03 02:40:43 +0000

    Comentarios

--- Progresso.java
+++ Progresso.java
@@ -1,15 +1,16 @@
 public abstract class Progresso {
+    // Atributos 
     protected Premiacao[] premiacoes;
     protected int premiacaoAtual;
-
+    // Construtor
     public Progresso() {
         this.premiacoes = new Premiacao[60];
         this.premiacaoAtual = 0;
     }
-
+    // Métodos abstratos para registrar vitória e entregar premiação
     public abstract void registrarVitoria();
     public abstract void entregarPremiacao();
-
+    // Enumeração de tipos de premiação
     protected enum Premiacao {
         BOOSTER_COMUM,
         BOOSTER_ESPECIAL
