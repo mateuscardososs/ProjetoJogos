8e07fec35424d0dd490095d25693206dbd195855
Breno Oliveira <99503617+Brelin27@users.noreply.github.com>
2023-12-03 02:32:16 +0000

    Comentarios

--- PasseDeBatalha.java
+++ PasseDeBatalha.java
@@ -1,13 +1,14 @@
 public class PasseDeBatalha {
     public static void main(String[] args) {
-       
+       // Instanciar e usar o PasseComum
         PasseComum passeComum = new PasseComum();
         passeComum.registrarVitoria();
         passeComum.entregarPremiacao();
         passeComum.finalizarPasse();
 
         System.out.println();
-
+        
+       // Instanciar e usar o PassePremium
         PassePremium passePremium = new PassePremium();
         passePremium.registrarVitoria();
         passePremium.entregarPremiacao();
