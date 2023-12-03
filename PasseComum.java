91c55e27163f66cf74fb5a7e42d61a7d68fc955c
Breno Oliveira <99503617+Brelin27@users.noreply.github.com>
2023-12-03 02:31:20 +0000

    Comentarios

--- PasseComum.java
+++ PasseComum.java
@@ -1,26 +1,26 @@
 public class PasseComum extends Progresso {
-
+    // Atributos
     private boolean passeFinalizado;
-
+    // Construtor
     public PasseComum() {
         super();
         this.passeFinalizado = false;
     }
-
+    // Método para registrar uma vitória no passe comum
     @Override
     public void registrarVitoria() {
         if (!passeFinalizado) {
             premiacaoAtual++;
         }
     }
-
+    // Método para entregar a premiação do passe comum
     @Override
     public void entregarPremiacao() {
         if (!passeFinalizado) {
             System.out.println("Booster comum aberto.");
         }
     }
-
+    // Método para finalizar o passe comum
     public void finalizarPasse() {
         this.passeFinalizado = true;
     }
