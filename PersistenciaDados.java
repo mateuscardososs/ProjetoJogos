0bc07df8b344d0e04db0e860e2ba036b22f977ad
Breno Oliveira <99503617+Brelin27@users.noreply.github.com>
2023-12-03 02:39:27 +0000

    Comentarios

--- PersistenciaDados.java
+++ PersistenciaDados.java
@@ -5,9 +5,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class PersistenciaDados {
-
+    // Nome do arquivo para persistência de dados
     private static final String NOME_ARQUIVO = "dadosUsuarios.txt";
-
+    // Método para salvar dados do usuário no arquivo
     public static void salvarDadosUsuario(String dados) {
         try {
             Files.writeString(Paths.get(NOME_ARQUIVO), dados);
@@ -15,7 +15,7 @@ public class PersistenciaDados {
             e.printStackTrace();
         }
     }
-
+    // Método para carregar dados do usuário do arquivo
     public static String carregarDadosUsuario() {
         StringBuilder dados = new StringBuilder();
 
